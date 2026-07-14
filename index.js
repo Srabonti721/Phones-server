e7ef64c8c31782bc27e5a3e5acbe6ed0aa0da5d6
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-15 01:18:46 +0600

    added phone data and single id to data

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const phones = require('./phones.json')
 const app = express();
 const port = 5000;
 
 app.get('/', (req,res)=>{
     res.send("hello this is ashs my phones server")
+})
+
+app.get('/phones', (req, res)=>{
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req,res)=>{
+    const id =req.params.id;
+    const parseId = parseInt(id);
+    const phone = phones.find(phoneData=>phoneData.id === parseId) || {};
+    res.send(phone)
+
 })
 
 app.listen(port, ()=>{
